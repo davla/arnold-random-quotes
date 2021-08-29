8f1fccc5b440d3bc885d12339b3f95b5cfc4ce65
Davide Laezza <truzzialrogo@gmx.com>
2021-08-29 22:26:25 +0200

    build: add setup script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # arnold-random-quotes
 Play random Arnold Schwarzenegger quotes when the proximity sensor triggers
+
+## Run
+
+## Setup
+
+Just run `setup.sh`. :smiley:
+
+The script will:
+
+- Install system dependencies
+- Create a Python3 virtual environment
+- Install pip dependencies
---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+# This script sets up the whole application
+
+########################################
+# System dependencies
+########################################
+
+# Python dependencies
+sudo apt install \
+    python3 \
+    python3-venv
+
+# Audio dependencies
+sudo apt install \
+    gir1.2-gstreamer-1.0 \
+    gstreamer1.0-gl \
+    gstreamer1.0-plugins-bad \
+    gstreamer1.0-plugins-base \
+    gstreamer1.0-plugins-good \
+    gstreamer1.0-plugins-ugly \
+    gstreamer1.0-pulseaudio \
+    python3-gi
+
+########################################
+# Python virtualenv
+########################################
+
+python3 -m venv --system-site-packages .venv
+
+########################################
+# Pip dependencies
+########################################
+
+. .venv/bin/activate
+pip install -r requirements.txt
+deactivate
