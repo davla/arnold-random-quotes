cce0663c4eb3df237060d40d73b7dfda15252d1a
Davide Laezza <truzzialrogo@gmx.com>
2021-08-31 19:42:34 +0200

    feature: add random audio file selection

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
+import os
+import random
 import sys
-from os.path import abspath
+from os import path
 
 from playsound import playsound
 
 play_audio_file = playsound
 
-if __name__ == "__main__":
-    file_path = abspath(sys.argv[1])
+
+def random_file(dir_path):
+    file_name = random.choice(os.listdir(dir_path))
+    return path.join(dir_path, file_name)
+
+
+def main(audio_files_dir):
+    random.seed()
+
+    audio_files_dir = path.abspath(audio_files_dir)
+    file_path = random_file(audio_files_dir)
 
     play_audio_file(file_path)
+
+
+if __name__ == "__main__":
+    try:
+        audio_files_dir = sys.argv[1]
+    except IndexError:
+        audio_files_dir = "./audio-files"
+
+    main(audio_files_dir)
